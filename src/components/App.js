8a751dc357edecb46879797da5cd89a4f838b3a7
Ian Wilson <iwilsonq@gmail.com>
2017-01-18 18:21:29 -0800

    bootstrapped with ReactReduxStarter

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+
+      </div>
+    );
+  }
+}
+
+export default connect(null)(App);
